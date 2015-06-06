7a1147b0f4f1d5741c9a3c3ff196154d9e4808ff
jilves <joosep@agileworks.eu>
2015-06-06 14:36:04 +0300

    default build and revision numbers

---
+++ Nuget-Publish.ps1
@@ -0,0 +1,17 @@
+﻿$projectNames = @('BankLinksDotNet', 'BanklinksDotNet.EstcardProvider', 'BanklinksDotNet.IPizzaProvider')
+$configurationNames = @('Nuget 4.5.1')
+
+function Delete-BuildFolders($baseDir)
+{
+    foreach($folderToDelete in @('bin', 'obj'))
+    {
+        $absoluteFolderPath = [System.IO.Path]::Combine($baseDir, $folderToDelete)
+        Remove-Item $absoluteFolderPath -Force -Recurse
+    }
+}
+
+foreach($projectName in $projectNames) 
+{
+    $projectDirectory = [System.IO.Path]::Combine($PSScriptRoot, $projectName)
+    Delete-BuildFolders $projectDirectory
+}
