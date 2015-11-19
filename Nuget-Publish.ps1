5745c08c21b00ca1c44195c8f8cd113fa7d32024
Joosep Ilves <ilves.joosep@outlook.com>
2015-11-19 18:30:25 +0200

    Automated nuget publish

--- Nuget-Publish.ps1
+++ Nuget-Publish.ps1
@@ -1,9 +1,25 @@
-﻿$projectNames = @('BankLinksDotNet', 'BanklinksDotNet.EstcardProvider', 'BanklinksDotNet.IPizzaProvider')
-$configurationNames = @('Nuget 4.5.1')
+﻿param ( 
+	[Parameter(Mandatory=$true,Position=1)]
+    [string]
+	$NuGetAPIKey,
+    [Parameter(Mandatory=$false, Position=2)]
+    [string]
+    $MsbuildExe = "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe",
+    [Parameter(Mandatory=$false, Position=3)]
+    [string]
+    # Assume nuget.exe is in Path variable
+    $NugetExe = "nuget"
+) 
+
+
+$projectNames = @('BankLinksDotNet', 'BanklinksDotNet.EstcardProvider', 'BanklinksDotNet.IPizzaProvider')
+
+
+$liveProjectconfigurationNames = @("Nuget 4.5.1")
 
 function Delete-BuildFolders($baseDir)
 {
-    foreach($folderToDelete in @('bin', 'obj'))
+    foreach($folderToDelete in @("bin", "obj"))
     {
         $absoluteFolderPath = [System.IO.Path]::Combine($baseDir, $folderToDelete)
         if(Test-Path $absoluteFolderPath)
@@ -13,9 +29,32 @@ function Delete-BuildFolders($baseDir)
     }
 }
 
+Get-ChildItem -Path $PSScriptRoot -Filter "*.nupkg" | Remove-Item
+
 foreach($projectName in $projectNames) 
 {
     $projectDirectory = [System.IO.Path]::Combine($PSScriptRoot, $projectName)
+
     Delete-BuildFolders $projectDirectory
-    # http://stackoverflow.com/questions/4677222/powershell-script-to-build-visual-studio-project
+
+    foreach($configName in $liveProjectconfigurationNames) 
+    {
+        $csProjFileFullPath = [System.IO.Path]::Combine($projectDirectory, $projectName + ".csproj")
+
+        & $MsbuildExe @($csProjFileFullPath, "/p:Configuration=$configName")
+
+        & $NugetExe @("pack", $csProjFileFullPath, "-Properties", "Configuration=$configName")
+
+        # We don't nuget push just yet, all project builds should succeed before proceeding
+    }
+}
+
+$projectNames | %{ 
+    $nuspecFile = Get-ChildItem -Path $PSScriptRoot -Filter "$_.1.*.nupkg" | Select-Object -First 1
+    $shouldProceed = Read-Host "Attempting to push: '$nuspecFile', Enter 'Yes' to proceed."
+    if($shouldProceed -ne 'Yes') {
+        Exit
+    }
+
+    & $NugetExe @("push", $nuspecFile, $NuGetAPIKey)
 }
